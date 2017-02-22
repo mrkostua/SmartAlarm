f9a50ae536ffda1bb3166fc9b8408f19f084693d
mrkostua <kprysiazhnyi@gmail.com>
2017-02-22 15:43:09 +0200

    MainMathAlarmActivty -> design changes, Add new functionality (deep sleep music)

--- app/src/main/java/com/example/mathalarm/Alarms/MathAlarm/MainMathAlarm.java
+++ app/src/main/java/com/example/mathalarm/Alarms/MathAlarm/MainMathAlarm.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -26,75 +27,99 @@ public class MainMathAlarm extends AppCompatActivity {
     public static final String ALARM_START_NEW = "alarm_start_new";
     public static final String TAG = "AlarmProcess";
 
-   private Button b1Line,b2Line,b3Line,b4Line;
-   private TextView tvPickedTime,tvChooseMusic, tvAlarmMessageText, tvChangeAlarmComplexity;
-   private EditText etGetAlarmMessageText;
-//values for setting on alarm
-private int pickedHour,pickedMinute;
-private boolean timePickerStatus =false;
-//values for setting music
-private int selectedMusic=0;
-private String[] musicList ;
-private boolean mpIsPlaying = false;
-private MediaPlayer mediaPlayer;
+    private TableRow trChangeTime, trChooseMusic, trChangeMessage, trChangeComplexity, trDeepSleepMusic;
+    private TextView tvPickedTime, tvChooseMusic, tvAlarmMessageText, tvChangeAlarmComplexity, tvDeepSleepMusic;
+    private EditText etGetAlarmMessageText;
+    //values for setting on alarm
+    private int pickedHour, pickedMinute;
+    private boolean timePickerStatus = false;
+    //values for setting music
+    private int selectedMusic = 0;
+    private String[] musicList;
+    private boolean mpIsPlaying = false;
+    private MediaPlayer mediaPlayer;
 
-private String alarmMessageText ="\"" +"Good morning sir" +"\"";
+    private String alarmMessageText = "\"" + "Good morning sir" + "\"";
 
-private String[] alarmComplexityList ;
-    private int selectedComplexityLevel =0;
+    private String[] alarmComplexityList;
+    private int selectedComplexityLevel = 0;
+
+    private int selectedDeepSleepMusic = 0;
+    private String[] deepSleepMusicList;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)  {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_math_alarm);
-        musicList =  getResources().getStringArray(R.array.music_list);
+
+        trChangeTime = (TableRow) findViewById(R.id.trChangeTime);
+        trChooseMusic = (TableRow) findViewById(R.id.trChooseMusic);
+        trChangeMessage = (TableRow) findViewById(R.id.trChangeMessage);
+        trChangeComplexity = (TableRow) findViewById(R.id.trChangeComplexity);
+        trDeepSleepMusic = (TableRow) findViewById(R.id.trDeepSleepMusic);
+
+        deepSleepMusicList = getResources().getStringArray(R.array.deepSleepMusic_list);
+
+        musicList = getResources().getStringArray(R.array.music_list);
         alarmComplexityList = getResources().getStringArray(R.array.alarm_complexity_list);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        trChangeComplexity.setBackgroundColor(getResources().getColor(R.color.black));
+        trChangeTime.setBackgroundColor(getResources().getColor(R.color.black));
+        trChangeMessage.setBackgroundColor(getResources().getColor(R.color.black));
+        trChooseMusic.setBackgroundColor(getResources().getColor(R.color.black));
+        trDeepSleepMusic.setBackgroundColor(getResources().getColor(R.color.black));
     }
 
     /**
      * @param view public methods for onClick events
      */
-        public void bSetAlarmTime_ClickMethod(View view) {
-    timePickerStatus=false;
-    tvPickedTime = (TextView) findViewById(R.id.tvSetTime);
-    b1Line = (Button) findViewById(R.id.b1Line);
-    //show timePickerDialog
-    TimePickerMethod().show();
-}
-
-        public void bChooseMusic_ClickMethod (View view) {
-        tvChooseMusic = (TextView) findViewById(R.id.tvChooseMusic);
-        b2Line = (Button) findViewById(R.id.b2Line);
-            //show alertDialog with music list
-            AlarmMusic_AlertDialogBuilder().create().show();
+    public void bSetAlarmTime_ClickMethod(View view) {
+        timePickerStatus = false;
+        tvPickedTime = (TextView) findViewById(R.id.tvSetTime);
+        //show timePickerDialog
+        TimePickerMethod().show();
     }
 
-        public void bAlarmMessageText_OnClickListener(View view) {
-        b3Line = (Button) findViewById(R.id.b3Line);
+    public void bChooseMusic_ClickMethod(View view) {
+        tvChooseMusic = (TextView) findViewById(R.id.tvChooseMusic);
+
+        //show alertDialog with music list
+        AlarmMusic_AlertDialogBuilder().create().show();
+    }
+
+    public void bAlarmMessageText_OnClickListener(View view) {
         tvAlarmMessageText = (TextView) findViewById(R.id.tvChangeMessage);
 
-            AlarmMessageText_AlertDialogBuilder().create().show();
+        AlarmMessageText_AlertDialogBuilder().create().show();
     }
 
-        public void bChangeAlarmComplexity_ClickMethod(View view) {
-            tvChangeAlarmComplexity = (TextView) findViewById(R.id.tvChangeComplexity);
-            b4Line = (Button) findViewById(R.id.b4Line);
+    public void bChangeAlarmComplexity_ClickMethod(View view) {
+        tvChangeAlarmComplexity = (TextView) findViewById(R.id.tvChangeComplexity);
 
-            AlarmComplexity_AlertDialogBuilder().create().show();
-        }
+        AlarmComplexity_AlertDialogBuilder().create().show();
+    }
 
-        public void bCreateMathAlarm_ClickMethod(View view) {
-        final CharSequence[] alarmSettingsItems = {pickedHour +" : " + pickedMinute, musicList[selectedMusic],
-                alarmComplexityList[selectedComplexityLevel],alarmMessageText};
+    public void bDeepSleepMusic_ClickMethod(View view) {
+        tvDeepSleepMusic = (TextView) findViewById(R.id.tvDeepSleepMusic);
+        AlarmDeepSleepMusic_AlertDialogBuilder().create().show();
+    }
+
+    public void bCreateMathAlarm_ClickMethod(View view) {
+        final CharSequence[] alarmSettingsItems = {pickedHour + " : " + pickedMinute, musicList[selectedMusic],
+                alarmComplexityList[selectedComplexityLevel], alarmMessageText, deepSleepMusicList[selectedDeepSleepMusic]};
         //Check if the time was Picked by user
-        if(timePickerStatus) {
+        if (timePickerStatus) {
             AlertDialog.Builder alertDialogAlarmPreview = new AlertDialog.Builder(this);
             alertDialogAlarmPreview.setTitle("Preview")
                     .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            MathAlarmPreview mathAlarmPreview = new MathAlarmPreview(MainMathAlarm.this,pickedHour,pickedMinute,selectedMusic,
-                                    selectedComplexityLevel,alarmMessageText);
+                            MathAlarmPreview mathAlarmPreview = new MathAlarmPreview(MainMathAlarm.this, pickedHour, pickedMinute, selectedMusic,
+                                    selectedComplexityLevel, alarmMessageText);
                             mathAlarmPreview.ConfirmAlarmPreview_Method();
                         }
                     })
@@ -109,7 +134,6 @@ private String[] alarmComplexityList ;
                         public void onClick(DialogInterface dialog, int which) {
                             //close alertDialog to clean display area for another( to show another alertDialog)
                             dialog.dismiss();
-
                             switch (which) {
                                 case 0:
                                     TimePickerMethod().show();
@@ -123,16 +147,18 @@ private String[] alarmComplexityList ;
                                 case 3:
                                     AlarmMessageText_AlertDialogBuilder().create().show();
                                     break;
-                                default:
-                                    Toast.makeText(MainMathAlarm.this,"how it is possible",Toast.LENGTH_SHORT).show();
+                                case 4:
+                                    AlarmDeepSleepMusic_AlertDialogBuilder().create().show();
                                     break;
                             }
                         }
                     }).create().show();
-        }
-        else
-            Toast.makeText(MainMathAlarm.this,"To create alarm please first set the alarm time",Toast.LENGTH_LONG).show();
+        } else
+            Toast.makeText(MainMathAlarm.this, "To create alarm please first set the alarm time", Toast.LENGTH_LONG).show();
     }
+
+
+
     /**
      * @return values of AlertDialog.Builder and TimePickerDialog
      */
@@ -152,19 +178,20 @@ private String[] alarmComplexityList ;
                 // change text of tvPickedTime to show picked time
                 if (minute < 10) {
                     tvPickedTime.setText("hour: " + hourOfDay + " minute: " + "0" + String.valueOf(minute));
-                } else{
+                } else {
                     tvPickedTime.setText("hour: " + hourOfDay + " minute: " + minute);
                 }
-                pickedHour=hourOfDay;
-                pickedMinute=minute;
-                timePickerStatus=true;
-                b1Line.setBackgroundColor(getResources().getColor(R.color.green_correct_choose));
+                pickedHour = hourOfDay;
+                pickedMinute = minute;
+                timePickerStatus = true;
+
+                trChangeTime.setBackgroundColor(getResources().getColor(R.color.green_correct_choose));
             }
-        },currentHour,currentMinute,false);
+        }, currentHour, currentMinute, false);
         return timePickerDialog;
     }
 
-    private AlertDialog.Builder AlarmComplexity_AlertDialogBuilder(){
+    private AlertDialog.Builder AlarmComplexity_AlertDialogBuilder() {
         AlertDialog.Builder alarmComplexity_alertDialog = new AlertDialog.Builder(MainMathAlarm.this);
         alarmComplexity_alertDialog.setTitle("complexity of the task")
                 .setSingleChoiceItems(R.array.alarm_complexity_list, 0, new DialogInterface.OnClickListener() {
@@ -177,34 +204,33 @@ private String[] alarmComplexityList ;
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         tvChangeAlarmComplexity.setText(alarmComplexityList[selectedComplexityLevel]);
-                        b4Line.setBackgroundColor(getResources().getColor(R.color.green_correct_choose));
+                        trChangeComplexity.setBackgroundColor(getResources().getColor(R.color.green_correct_choose));
                     }
                 });
         return alarmComplexity_alertDialog;
     }
 
-    private AlertDialog.Builder AlarmMessageText_AlertDialogBuilder(){
+    private AlertDialog.Builder AlarmMessageText_AlertDialogBuilder() {
         AlertDialog.Builder adBuilder_GetMessageForAlarm = new AlertDialog.Builder(MainMathAlarm.this);
 
-        RelativeLayout relativeLayoutView = (RelativeLayout) getLayoutInflater().inflate(R.layout.custom_layout_bchange_text,null);
+        RelativeLayout relativeLayoutView = (RelativeLayout) getLayoutInflater().inflate(R.layout.custom_layout_bchange_text, null);
         etGetAlarmMessageText = (EditText) relativeLayoutView.findViewById(R.id.etGetAlarmMessageText);
 
         adBuilder_GetMessageForAlarm.setView(relativeLayoutView)
                 .setTitle("Write a message for yourself")
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which)
-                    {
+                    public void onClick(DialogInterface dialog, int which) {
                         alarmMessageText = etGetAlarmMessageText.getText().toString();
-                        String defaultAlarmMessageText = "\"" +"Good morning sir" +"\"";
-                        if(!alarmMessageText.equals("")) {
-                            if(alarmMessageText.length()<=15)
+                        String defaultAlarmMessageText = "\"" + "Good morning" + "\"";
+                        if (!alarmMessageText.equals("")) {
+                            if (alarmMessageText.length() <= 15)
                                 tvAlarmMessageText.setText(alarmMessageText);
                             else
-                                tvAlarmMessageText.setText(alarmMessageText.substring(0,12)+"...");
+                                tvAlarmMessageText.setText(alarmMessageText.substring(0, 12) + "...");
+                            trChangeMessage.setBackgroundColor(getResources().getColor(R.color.green_correct_choose));
                         } else {
-                            tvAlarmMessageText.setText(alarmMessageText=defaultAlarmMessageText);
-                            b3Line.setBackgroundColor(getResources().getColor(R.color.green_correct_choose));
+                            tvAlarmMessageText.setText(alarmMessageText = defaultAlarmMessageText);
                         }
                     }
                 });
@@ -214,10 +240,9 @@ private String[] alarmComplexityList ;
     private AlertDialog.Builder AlarmMusic_AlertDialogBuilder() {
         AlertDialog.Builder alertDialog_ChooseMusic = new AlertDialog.Builder(MainMathAlarm.this);
         alertDialog_ChooseMusic.setTitle("Choose music for Alarm")
-                .setSingleChoiceItems(R.array.music_list,0, new DialogInterface.OnClickListener() {
+                .setSingleChoiceItems(R.array.music_list, 0, new DialogInterface.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which)
-                    {
+                    public void onClick(DialogInterface dialog, int which) {
                         selectedMusic = which;
                         Context getContext = getApplicationContext();
                         int musicPackedName = getContext.getResources().getIdentifier(musicList[which], "raw",
@@ -228,12 +253,13 @@ private String[] alarmComplexityList ;
                                 mediaPlayer = MediaPlayer.create(MainMathAlarm.this, musicPackedName);
                                 mediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
                                     public boolean onError(MediaPlayer mp, int what, int extra) {
-                                        Log.i(MainMathAlarm.TAG,"MathAlarmService Error occurred while playing audio.");
+                                        Log.i(MainMathAlarm.TAG, "MathAlarmService Error occurred while playing audio.");
                                         mp.stop();
                                         mp.release();
                                         mediaPlayer = null;
                                         return true;
-                                    }});
+                                    }
+                                });
                                 mediaPlayer.start();
                             } else {
                                 mpIsPlaying = true;
@@ -242,30 +268,50 @@ private String[] alarmComplexityList ;
                                 mediaPlayer = MediaPlayer.create(MainMathAlarm.this, musicPackedName);
                                 mediaPlayer.start();
                             }
-                        }catch (Exception e) {
+                        } catch (Exception e) {
                             Log.i(MainMathAlarm.TAG, "MathAlarmService " + " AlarmStartPlayingMusic error" + e.getMessage());
                         }
                     }
                 }).setPositiveButton("Good", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-            try {
-                if (mpIsPlaying) {
-                    mediaPlayer.stop();
-                    //after reset(mediaPlayer need to be initialize again be setting data source...
-                    mediaPlayer.reset();
-                    tvChooseMusic.setText(musicList[selectedMusic]);
-                } else {
-                    int defaultMusicNumber = 0;
-                    tvChooseMusic.setText(musicList[defaultMusicNumber]);
+                try {
+                    if (mpIsPlaying) {
+                        mediaPlayer.stop();
+                        //after reset(mediaPlayer need to be initialize again be setting data source...
+                        mediaPlayer.reset();
+                        tvChooseMusic.setText(musicList[selectedMusic]);
+                        trChooseMusic.setBackgroundColor(getResources().getColor(R.color.green_correct_choose));
+                    } else {
+                        int defaultMusicNumber = 0;
+                        tvChooseMusic.setText(musicList[defaultMusicNumber]);
+                    }
+                } catch (Exception e) {
+                    Log.i(MainMathAlarm.TAG, "MathAlarmService " + " AlarmStartPlayingMusic error" + e.getMessage());
                 }
-                b2Line.setBackgroundColor(getResources().getColor(R.color.green_correct_choose));
-            }catch (Exception e) {
-                Log.i(MainMathAlarm.TAG, "MathAlarmService " + " AlarmStartPlayingMusic error" + e.getMessage());
-            }
             }
         });
 
         return alertDialog_ChooseMusic;
     }
+
+    private AlertDialog.Builder AlarmDeepSleepMusic_AlertDialogBuilder() {
+        AlertDialog.Builder alertDialog_DeepSleepMusic = new AlertDialog.Builder(MainMathAlarm.this);
+        alertDialog_DeepSleepMusic.setTitle("Choose music for Alarm")
+                .setSingleChoiceItems(R.array.deepSleepMusic_list, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        selectedDeepSleepMusic=which;
+                    }
+                }).setPositiveButton("Good", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tvDeepSleepMusic.setText(deepSleepMusicList[selectedDeepSleepMusic]);
+                trDeepSleepMusic.setBackgroundColor(getResources().getColor(R.color.green_correct_choose));
+            }
+        });
+        return alertDialog_DeepSleepMusic;
+    }
+
+
 }
