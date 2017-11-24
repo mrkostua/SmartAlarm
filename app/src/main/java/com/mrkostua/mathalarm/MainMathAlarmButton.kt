4e2e53434e31e6a80b095bd586d5da1328c9a43c
mrkostua <kprysiazhnyi@gmail.com>
2017-11-24 21:31:28 +0200

    Method for showing current day of the week. Kotlin and Java 8 Stream, Lambda.

--- app/src/main/java/com/mrkostua/mathalarm/MainMathAlarmButton.kt
+++ app/src/main/java/com/mrkostua/mathalarm/MainMathAlarmButton.kt
@@ -5,12 +5,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
-import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.RelativeLayout
 import android.widget.TextView
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * @author Kostiantyn on 21.11.2017.
@@ -21,8 +21,9 @@ class MainMathAlarmButton : AppCompatActivity() {
     private lateinit var rlButtonLayout: RelativeLayout
     private var lastAlarmData: LastAlarmData = object : LastAlarmData(this) {}
 
-
     private lateinit var tvAlarmTime: TextView
+
+    private val listDaysOfWeekViews: ArrayList<TextView> = object : ArrayList<TextView>(7) {}
 
 
     /**
@@ -51,9 +52,10 @@ class MainMathAlarmButton : AppCompatActivity() {
         rlButtonLayout = findViewById(R.id.rlButtonLayout) as RelativeLayout
 
         tvAlarmTime = findViewById(R.id.tvAlarmTime) as TextView
+
     }
 
-    private fun intializeAlarm() {
+    private fun initializeAlarm() {
         if (lastAlarmData.alarmHours != 0) {
             tvAlarmTime.text = Integer.toString(lastAlarmData.alarmHours) + " : " + Integer.toString(lastAlarmData.alarmMinutes)
 
@@ -63,11 +65,31 @@ class MainMathAlarmButton : AppCompatActivity() {
 
     }
 
-
+    //TODO Koltin is there still need for butterKnife library ?
     private fun showDayOfTheWeek() {
         val calendar: Calendar = Calendar.getInstance()
         calendar.timeInMillis = System.currentTimeMillis()
 
+        initializeDaysOfWeekViews()
+
+        listDaysOfWeekViews.forEachIndexed{indexOfDay, dayView ->
+            if(indexOfDay == calendar.get(Calendar.DAY_OF_WEEK))
+                underlineDayOfWeek(dayView)
+        }
+
+      /*  listDaysOfWeekViews.forEach { dayView ->
+            if(listDaysOfWeekViews.indexOf(dayView) == calendar.get(Calendar.DAY_OF_WEEK)){
+                underlineDayOfWeek(dayView)
+
+            }
+        }
+
+        for (dayView in listDaysOfWeekViews) {
+
+        }
+        // third : using basic for() loop
+
+        //ot switch case
         when (calendar.get(Calendar.DAY_OF_WEEK)) {
             0 -> {
 
@@ -91,25 +113,33 @@ class MainMathAlarmButton : AppCompatActivity() {
 
             }
 
-
-
-        }
+        }*/
 
     }
 
-    private fun underlineDayOfWeek(tvDayOfWeek : TextView){
+    private fun initializeDaysOfWeekViews() {
+        listDaysOfWeekViews.add(findViewById(R.id.tvMonday) as TextView)
+        listDaysOfWeekViews.add(findViewById(R.id.tvTuesday) as TextView)
+        listDaysOfWeekViews.add(findViewById(R.id.tvWednesday) as TextView)
+        listDaysOfWeekViews.add(findViewById(R.id.tvThursday) as TextView)
+        listDaysOfWeekViews.add(findViewById(R.id.tvFriday) as TextView)
+        listDaysOfWeekViews.add(findViewById(R.id.tvSaturday) as TextView)
+        listDaysOfWeekViews.add(findViewById(R.id.tvSunday) as TextView)
+
+    }
+
+
+    private fun underlineDayOfWeek(tvDayOfWeek: TextView) {
+        val ssContent: SpannableString = object : SpannableString("Content") {}
+        ssContent.setSpan(object : UnderlineSpan() {}, 0, ssContent.length, 0)
+        tvDayOfWeek.text = ssContent
+
+        //todo setTextAppearance(int) is only for api 23 and higher, find method for lower API
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             tvDayOfWeek.setTextAppearance(R.style.ChosenDayOfTheWeek_TextTheme)
         } else {
-            tvDayOfWeek.setTextAppearance(this,R.style.ChosenDayOfTheWeek_TextTheme)
+            @Suppress("DEPRECATION")
+            tvDayOfWeek.setTextAppearance(this, R.style.ChosenDayOfTheWeek_TextTheme)
         }
-        
-        val ssContent : SpannableString = object : SpannableString("Content"){}
-
-        ssContent.setSpan(object : UnderlineSpan(){}, 0, ssContent.length, 0 )
-
-        tvDayOfWeek.text = ssContent
-
-
     }
 }
