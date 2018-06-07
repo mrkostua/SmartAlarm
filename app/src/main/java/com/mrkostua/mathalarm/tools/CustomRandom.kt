51a97184856982bbadab28bbf739052c5144099f
mrkostua <kprysiazhnyi@gmail.com>
2018-06-07 19:13:53 +0200

    working on implementation of getUniqueRandomBorderedValues()

--- app/src/main/java/com/mrkostua/mathalarm/tools/CustomRandom.kt
+++ app/src/main/java/com/mrkostua/mathalarm/tools/CustomRandom.kt
@@ -1,6 +1,8 @@
 package com.mrkostua.mathalarm.tools
 
+import android.support.annotation.VisibleForTesting
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * @author Kostiantyn Prysiazhnyi on 6/7/2018.
@@ -26,11 +28,15 @@ object CustomRandom {
     }
 
     fun getUniqueRandomBorderedValues(minBound: Int, maxInclusiveBound: Int, size: Int = 1, bordersInDP: Int): ArrayList<Int> {
-        if (size * ((bordersInDP / 2) * 2) + size > (maxInclusiveBound - minBound + 1)) {
+        if (bordersInDP < 2) {
+            throw UnsupportedOperationException("Padding in 1 dimensional space must be > 1 (null-> result <- padding)")
+        }
+        if (size * getClosestEvenNumber(bordersInDP) + size > (maxInclusiveBound - minBound + 1)) {
             throw UnsupportedOperationException("getUniqueRandomValues impossible to generate more unique values with borders," +
                     " than (maxInclusiveBound + 1) - minBound")
         }
         val oneSideBorder = bordersInDP / 2
+
         val uniqueRandomListWithPadding = ArrayList<Int>()
         val uniqueRandomList = ArrayList<Int>()
         val bufferArray = ArrayList<Int>()
@@ -39,14 +45,18 @@ object CustomRandom {
 
         while (uniqueRandomList.size < size) {
             generatedValue = random.nextInt((maxInclusiveBound - minBound) + 1) + minBound
+            println("getUniqBV() generatedValue $generatedValue")
             if (!uniqueRandomListWithPadding.contains(generatedValue)) {
                 if (generatedValue - minBound >= oneSideBorder && maxInclusiveBound - generatedValue >= oneSideBorder) {
                     for (i in generatedValue - oneSideBorder..generatedValue + oneSideBorder) {
                         bufferArray.add(i)
                     }
                     if (!containsAtLeastOne(uniqueRandomListWithPadding, bufferArray)) {
-                        uniqueRandomList.add(generatedValue)
-                        uniqueRandomListWithPadding.addAll(bufferArray)
+                        if (isThereMoreEmptySpaceToGenerate(bufferArray, getIntegerListCopy(uniqueRandomListWithPadding), IntRange(minBound, maxInclusiveBound).toList() as ArrayList<Int>, size)) {
+                            uniqueRandomList.add(generatedValue)
+                            println("getUniqBV() acceptedValue is: $generatedValue")
+                            uniqueRandomListWithPadding.addAll(bufferArray)
+                        }
 
                     }
                     bufferArray.clear()
@@ -55,6 +65,74 @@ object CustomRandom {
 
         }
         return uniqueRandomList
+    }
+
+    private fun getIntegerListCopy(array: ArrayList<Int>): ArrayList<Int> {
+        val result = ArrayList<Int>()
+        result.addAll(array)
+        return result
+    }
+
+    private fun isThereMoreEmptySpaceToGenerate(generatedElements: ArrayList<Int>, savedGeneratedElements: ArrayList<Int>, initialFreeElementsList: ArrayList<Int>, wantedResutSize: Int): Boolean {
+        val oneTimeGenerationWeigh = generatedElements.size
+        var restAmountOfElementsToGenerate = (wantedResutSize - savedGeneratedElements.size / oneTimeGenerationWeigh) - 1 //1 is our generatedElements
+        val oneSideBorder = (generatedElements.size - 1) / 2
+        initialFreeElementsList.sort()
+        var waitingIndex = 0
+        //simple solution savedGenElem is empty
+        savedGeneratedElements.addAll(generatedElements)
+        savedGeneratedElements.forEach {
+            initialFreeElementsList.remove(it)
+        }
+        for (i in 0..initialFreeElementsList.lastIndex) {
+            println("main loop $i")
+            if (waitingIndex > 0) {
+                --waitingIndex
+                continue
+            }
+            if (i < oneSideBorder) {
+                continue
+            }
+            println("main loop after skipping ${initialFreeElementsList[i]}")
+
+            if (checkLeftElements(i, initialFreeElementsList, oneSideBorder)) {
+                if (initialFreeElementsList.lastIndex - oneSideBorder >= i && checkRightElements(i, initialFreeElementsList, oneSideBorder)) {
+                    //decrementing our result variable
+                    if (--restAmountOfElementsToGenerate <= 0) {
+                        break
+                    }
+                    //checking if it is not the end of the loop and skipping padding elements in the loop
+                    if (i + oneSideBorder * 2 + 1 > initialFreeElementsList.lastIndex) {
+                        waitingIndex = oneSideBorder * 2 + 1
+                    } else {
+                        break
+                    }
+
+                }
+            }
+            initialFreeElementsList[i]
+        }
+        return restAmountOfElementsToGenerate <= 0
+    }
+
+    private fun checkLeftElements(currentElementIndex: Int, arrayList: ArrayList<Int>, paddingAmount: Int): Boolean {
+        for (i in 1..paddingAmount) {
+            if (arrayList[currentElementIndex] - i == arrayList[currentElementIndex - i]) {
+                continue
+            }
+            return false
+        }
+        return true
+    }
+
+    private fun checkRightElements(currentElementIndex: Int, arrayList: ArrayList<Int>, paddingAmount: Int): Boolean {
+        for (i in 1..paddingAmount) {
+            if (arrayList[currentElementIndex] + i == arrayList[currentElementIndex + i]) {
+                continue
+            }
+            return false
+        }
+        return true
     }
 
     private fun <T> containsAtLeastOne(array: ArrayList<T>, what: ArrayList<T>): Boolean {
@@ -66,4 +144,9 @@ object CustomRandom {
         return false
     }
 
+    private fun getClosestEvenNumber(from: Int) = ((from / 2) * 2)
+
+    @VisibleForTesting
+    fun getTestingIsThereMoreEmptySpaceToGenerate(generatedElements: ArrayList<Int>, savedGeneratedElements: ArrayList<Int>, initialFreeElementsList: ArrayList<Int>, wantedResultSize: Int) =
+            isThereMoreEmptySpaceToGenerate(generatedElements, savedGeneratedElements, initialFreeElementsList, wantedResultSize)
 }
--- app/src/test/java/com/mrkostua/mathalarm/tools/CustomRandomTest.kt
+++ app/src/test/java/com/mrkostua/mathalarm/tools/CustomRandomTest.kt
@@ -1,8 +1,11 @@
 package com.mrkostua.mathalarm.tools
 
 import junit.framework.Assert
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
 import org.junit.Test
 import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * @author Kostiantyn Prysiazhnyi on 6/7/2018.
@@ -21,7 +24,7 @@ class CustomRandomTest {
         var check = false
         try {
             CustomRandom.getUniqueRandomValues(0, 10, 12)
-        } catch (e: UnsupportedOperationException) {
+        } catch (e: Exception) {
             check = true
         }
         Assert.assertTrue("for incorrect fun arguments exception wasn't thrown", check)
@@ -51,7 +54,7 @@ class CustomRandomTest {
         var check = false
         try {
             CustomRandom.getUniqueRandomBorderedValues(0, 5, 2, 4)
-        } catch (e: UnsupportedOperationException) {
+        } catch (e: Exception) {
             check = true
         }
         Assert.assertTrue("for incorrect fun arguments exception wasn't thrown", check)
@@ -61,26 +64,33 @@ class CustomRandomTest {
 
     @Test
     fun getUniqueRandomBorderedValues() {
-        val testData = arrayOf(Pair(0, 3), Pair(20, 100), Pair(5, 25))
-        val testSizePaddingData = arrayOf(Pair(2, 1), Pair(6, 4), Pair(5, 5))
+        val testBorderData = arrayOf(Pair(10,25),Pair(0, 5), Pair(20, 100), Pair(5, 25))
+        val testSizePaddingData = arrayOf(Pair(5,2),Pair(2, 2), Pair(6, 4), Pair(5, 5))
         var result: ArrayList<Int>
         val uniqueSet = HashSet<Int>()
-        Assert.assertEquals("wrong implementation of the test data", testData.size, testSizePaddingData.size)
+        var oneSideBorder: Int
+        Assert.assertEquals("wrong implementation of the test data", testBorderData.size, testSizePaddingData.size)
 
-        for (i in 0 until testData.size) {
-            result = CustomRandom.getUniqueRandomBorderedValues(testData[i].first, testData[i].second, testSizePaddingData[i].first, testSizePaddingData[i].second)
+        for (i in 0 until testBorderData.size) {
+            result = CustomRandom.getUniqueRandomBorderedValues(testBorderData[i].first, testBorderData[i].second, testSizePaddingData[i].first, testSizePaddingData[i].second)
             result.sort()
+
+            oneSideBorder = testSizePaddingData[i].second / 2
+            //testing
+            println("\noneSideBorder :$oneSideBorder")
+            print("initial Data")
+            result.forEach { print(it.toString() + " ") }
             result.forEachIndexed { index, j ->
-                Assert.assertTrue("generated value is from the wrong range", j in testData[i].first..testData[i].second)
+                Assert.assertTrue("generated value $j is from the wrong range", j in testBorderData[i].first..testBorderData[i].second)
                 uniqueSet.add(j)
                 when (index) {
-                    0 -> Assert.assertTrue("first generated value have bad padding",
-                            (j - testSizePaddingData[i].second / 2 >= 0) && (j + testSizePaddingData[i].second / 2 < result[j + 1] - testSizePaddingData[i].second / 2))
-                    result.lastIndex -> Assert.assertTrue("last generated value have bad padding",
-                            (j - testSizePaddingData[i].second / 2 > result[j - 1] + testSizePaddingData[i].second / 2) && (j + testSizePaddingData[i].second / 2 <= testData[i].second))
+                    0 -> Assert.assertTrue("first generated value have bad padding for $j",
+                            (j - oneSideBorder >= testBorderData[i].first) && (j + oneSideBorder < result[index + 1] - oneSideBorder))
+                    result.lastIndex -> Assert.assertTrue("last generated value have bad padding for $j",
+                            (j - oneSideBorder > result[index - 1] + oneSideBorder) && (j + oneSideBorder <= testBorderData[i].second))
                     else -> {
                         Assert.assertTrue("inner generated value have bad padding",
-                                (j - testSizePaddingData[i].second / 2 > result[j - 1] + testSizePaddingData[i].second / 2) && (j + testSizePaddingData[i].second / 2 < result[j + 1] - testSizePaddingData[i].second / 2))
+                                (j - oneSideBorder > result[index - 1] + oneSideBorder) && (j + oneSideBorder < result[index + 1] - oneSideBorder))
 
                     }
                 }
@@ -89,6 +99,18 @@ class CustomRandomTest {
             Assert.assertEquals("generated array contains duplicates", uniqueSet.size, result.size)
             uniqueSet.clear()
             result.clear()
+            break
         }
+    }
+
+    /**
+     * 2 ways to test private method (reflection in case of some class we can't change) and simple visible getter
+     */
+    @Test
+    fun isThereMoreEmptySpaceToGenerate() {
+        assertTrue(CustomRandom.getTestingIsThereMoreEmptySpaceToGenerate(arrayListOf(1, 2, 3), ArrayList(), arrayListOf(0, 1, 2, 3, 4, 5, 6, 7), 2))
+        assertFalse(CustomRandom.getTestingIsThereMoreEmptySpaceToGenerate(arrayListOf(1, 2, 3), ArrayList(), arrayListOf(0, 1, 2, 3, 4, 5), 2))
+        assertFalse(CustomRandom.getTestingIsThereMoreEmptySpaceToGenerate(arrayListOf(1, 2, 3), arrayListOf(4, 5, 6), arrayListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 4))
+        assertTrue(CustomRandom.getTestingIsThereMoreEmptySpaceToGenerate(arrayListOf(8, 9, 10), arrayListOf(4, 5, 6), arrayListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 3))
     }
 }
